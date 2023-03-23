50ab572ccbd794bacd7b9d8de25fe8bc9b20e3aa
Hemant <hrsmart2150@gmail.com>
2023-03-23 17:47:16 +0530

    Successfully build popular and nearby jobs section

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
-import { View, Text, ScrollView, SafeAreaView } from 'react-native'
-import { Stack, useRouter } from 'expo-router'
-import { useState } from 'react';
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
 
-import { COLORS, icons, images, SIZES } from '../constants'
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+import {
+    Nearbyjobs,
+    Popularjobs,
+    ScreenHeaderBtn,
+    Welcome
+} from "../components";
+import { COLORS, icons, images, SIZES } from "../constants";
 
 const Home = () => {
-    const router = useRouter();
+    const router = useRouter()
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -14,26 +19,32 @@ const Home = () => {
                 options={{
                     headerStyle: { backgroundColor: COLORS.lightWhite },
                     headerShadowVisible: false,
-                    headerLeft: () => {
-                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-                    },
-                    headerRight: () => {
-                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-                    },
-                    headerTitle: ""
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+                    ),
+                    headerTitle: "",
                 }}
             />
 
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={{ flex: 1, padding: SIZES.medium }}>
-                    <Welcome />
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }}
+                >
+                    <Welcome
+                    />
 
                     <Popularjobs />
                     <Nearbyjobs />
                 </View>
             </ScrollView>
         </SafeAreaView>
-    )
-}
+    );
+};
 
 export default Home;
